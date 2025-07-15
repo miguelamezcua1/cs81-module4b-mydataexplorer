a482acd33faaa68dbab55a1e8ad35dca31eceea2
miguelamezcua1 <amezcua_miguel01@student.smc.edu>
2025-07-15 03:08:22 -0700

    Added weekly data and predictions

---
+++ myDataJournal.js
@@ -0,0 +1,18 @@
+// GitHub URL: https://github.com/miguelamezcua1/cs81-module4b-mydataexplorer
+
+// Weekly personal data journal
+const weekData = [
+  { day: "Monday", sleepHours: 6.5, screenTime: 7, mood: "tired", caffeineIntake: 2, focusLevel: 5 },
+  { day: "Tuesday", sleepHours: 7, screenTime: 6, mood: "focused", caffeineIntake: 1, focusLevel: 7 },
+  { day: "Wednesday", sleepHours: 5.5, screenTime: 8, mood: "distracted", caffeineIntake: 3, focusLevel: 4 },
+  { day: "Thursday", sleepHours: 7.5, screenTime: 5, mood: "productive", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Friday", sleepHours: 6, screenTime: 6.5, mood: "okay", caffeineIntake: 2, focusLevel: 6 },
+  { day: "Saturday", sleepHours: 8, screenTime: 4, mood: "relaxed", caffeineIntake: 0, focusLevel: 7 },
+  { day: "Sunday", sleepHours: 7.5, screenTime: 3, mood: "relaxed", caffeineIntake: 0, focusLevel: 6 }
+];
+
+//  PREDICTIONS 
+
+// I think Wednesday had the most screen time (I was on YouTube a lot).
+// Best focus day is probably Thursday. I remember getting a lot done.
+// I don’t think more caffeine helped my focus, maybe the opposite.
